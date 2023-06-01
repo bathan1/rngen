2cd1615e9c68ceb5f2f846628cfbae3dc4fbf0d8
Nathan <side_nathan@hotmail.com>
2023-06-01 17:48:37 -0400

    Add basic control flow for loop

--- generator.py
+++ generator.py
@@ -1,7 +1,22 @@
 import random
 
 print("Welcome to random number generator!")
-lower_bound = int(input("Enter desired lower bound of your randomly generated number: "))
-upper_bound = int(input("Enter desired upper (exclusive) bound of your randomly generated number: "))
-random_num = random.randrange(lower_bound, upper_bound)
-print("Your random number is", random_num)
+done = False
+while not done:
+    lower_bound = int(input("Enter desired lower bound of your randomly generated number: "))
+    upper_bound = int(input("Enter desired upper (exclusive) bound of your randomly generated number: "))
+    random_num = random.randrange(lower_bound, upper_bound)
+    print("Your random number is", random_num)
+
+    prompt = False
+    while not prompt:
+        loop = input("Need another number? (y/n) ")
+        loop = loop.lower()
+        if loop == 'y':
+            prompt = True
+            continue
+        if loop == 'n':
+            done = True
+            break
+        else:
+            print("You need to enter a valid answer!")
